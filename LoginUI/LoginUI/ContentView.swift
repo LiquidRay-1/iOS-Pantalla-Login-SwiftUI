c40d9c32ebf2fa3eabdb5cd0ee03cf423e68930c
Iván Tortosa <152330066+LiquidRay-1@users.noreply.github.com>
2024-02-15 21:27:17 +0100

    Add files via upload

--- LoginUI/LoginUI/ContentView.swift
+++ LoginUI/LoginUI/ContentView.swift
@@ -9,10 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        ZStack {
+            Color
+                .cyan
+                .ignoresSafeArea(.all)
             
+            Circle()
+                .foregroundStyle(.bar)
+                .frame(width: 700)
+            
+            Circle()
+                .foregroundStyle(.white)
+                .frame(width: 500)
+            
+            VStack{
+                Text("Iniciar sesión")
+                    .font(.largeTitle)
+                
+                TextField("Nombre de usuario", text:
+                        .constant(""))
+                        .frame(width: 300, height: 50)
+                        .background(.bar)
+                        .clipShape(.buttonBorder)
+                
+                SecureField("Contraseña", text:
+                        .constant(""))
+                        .frame(width: 300, height: 50)
+                        .background(.bar)
+                        .clipShape(.buttonBorder)
+                
+                HStack{
+                    Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                        
+                    }.frame(width: 50)
+                        .foregroundStyle(.blue)
+                    
+                    Text("Recordar contraseña")
+                        .frame(minWidth: 30)
+                }
+                
+                Button("Login") {
+                    
+                }
+                .padding()
+                .background(.cyan)
+                .foregroundStyle(.white)
+                .clipShape(.buttonBorder)
+                
+                Text("Recuperar Contraseña")
+            }
         }
-        .padding()
     }
 }
 
