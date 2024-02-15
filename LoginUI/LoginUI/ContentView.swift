b73affe54af1c177ab1e856c77dc4a6443cc2402
Iván Tortosa <152330066+LiquidRay-1@users.noreply.github.com>
2024-02-15 20:28:11 +0100

    Add files via upload

---
+++ LoginUI/LoginUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginUI
+//
+//  Created by dam2 on 15/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginUI/LoginUI/LoginUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginUIApp.swift
+//  LoginUI
+//
+//  Created by dam2 on 15/2/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
